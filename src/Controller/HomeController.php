0dbc31bbd32890c0b91ba128265d457d0e389126
Chloebert <berthier.chloe@wanadoo.fr>
2022-07-21 11:15:55 +0200

    cat of the month card

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\CatPictureRepository;
+use App\Repository\CatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CatRepository $catRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        return $this->render('home/index.html.twig', [
+            'catOfTheMonth' => $catRepository->findBy(['catOfTheMonth' => true]),
+        ]);
     }
 }
