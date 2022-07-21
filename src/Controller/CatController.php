a733ca80cc4dc39d55cec2af10dbf1e3da1deb11
Chloebert <berthier.chloe@wanadoo.fr>
2022-07-21 12:33:42 +0200

    création de page controller et style

--- src/Controller/CatController.php
+++ src/Controller/CatController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/cat')]
+#[Route('admin/cat')]
 class CatController extends AbstractController
 {
     #[Route('/', name: 'app_cat_index', methods: ['GET'])]
